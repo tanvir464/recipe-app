0a7dd5d87f72629a68161c6adf3f0d70347fe752
Tanvir Ahmed <100764069+tanvir464@users.noreply.github.com>
2024-09-12 19:10:34 +0600

    user menu fix

--- recipe/settings.py
+++ recipe/settings.py
@@ -127,7 +127,7 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST = 'smtp.office365.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
